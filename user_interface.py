372cd70207d25b8c5d04f82beb01ae5ddaf607f7
JesseShen27 <jesse.o.shen@gmail.com>
2023-01-12 15:03:33 -0500

    user_interface can now get username and tag through one input

--- user_interface.py
+++ user_interface.py
@@ -1,1 +1,49 @@
+import requests
+import ctypes
 
+# First task is to ask for user Information
+userInput = input("Enter Riot ID (\"example#0000\"): ")
+
+# valid input == 0 if invalid, anything else means valid
+
+validInput = 0
+
+if (len(userInput) > 22 or len(userInput) < 8):
+    print("Invalid Riot ID")
+else:
+    # Riot ID max name length 16 chars
+    userName = ("%" * 16)
+    # Riot ID max tag length 5 chars
+    userTag = ("%" * 5)
+
+    # Necessary Index values for string manipulation of tag and username
+    tagIndex = 0
+    nameIndex = 0
+    #loops through string looking for '#'
+    for i in range(len(userInput)):
+        # found '#' begin placing characters in tag
+        if (userInput[i] == "#"):
+            # loop to place characters from input into tag string
+            for j in range(len(userInput) - i - 1):
+                # if to check for last index
+                if (i == len(userInput) - 1):
+                    break
+                else:
+                    userTag = userTag[:tagIndex] + userInput[i + j + 1] + userTag[tagIndex:]
+                    tagIndex += 1
+            break
+        # Otherwise place characters from input in username
+        else:
+            userName = userName[:nameIndex] + userInput[i] + userName[nameIndex:]
+            nameIndex += 1
+
+    userTag = userTag.strip("%")
+    userName = userName.strip("%")
+
+    print("\"" + userName + "\"")
+    print("\"" + userTag + "\"")
+
+
+    # userTag and userName should be good to use now
+
+    
