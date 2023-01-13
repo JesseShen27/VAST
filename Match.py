5bf91c7efa989f3aa83424f2d449befe265f5dc5
JesseShen27 <94657588+JesseShen27@users.noreply.github.com>
2023-01-13 10:23:36 -0500

    Match Class edits before editing repository

---
+++ Match.py
@@ -0,0 +1,10 @@
+class Match:
+
+    def __init__(self):
+        # two arrays of size 5 representing team players in each index
+        self.blueTeam = [None] * 5
+        self.redTeam = [None] * 5
+
+        # String that will represent the team color of the user
+        self.userTeamColor = None
+    
