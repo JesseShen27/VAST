71fab61f3aa5d5b560e24c475c629563782e9cf7
Danny Disman <dismandanny@gmail.com>
2023-05-30 17:00:45 -0400

    Fixed how the loading was taking too long for the home page now on the login page

--- src/flsktest.py
+++ src/flsktest.py
@@ -5,25 +5,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    database1 = process_data("clu1#NA1", "na")
-    name = request.form.get('name')
-    b1_avg_wr = str(win_percentage(database1.b1))
     return render_template("index.html")
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        # we have the user inputted data now stored in the user and id variables
+        # we have the user inputted data now stored in the user and region variables
         user = request.form["nm"]
-        id = request.form["id"]
-        print(user + id)
-        return redirect(url_for("user", usr = user, id = id))
+        region = request.form["region"]
+        database1 = process_data(user, region)
+        b1_avg_wr = str(win_percentage(database1.b1))
+        return redirect(url_for("user", usr = user, region = region))
     else:
         return render_template("login.html")
 
-@app.route("/<usr><id>")
-def user(usr, id):
-    return f"<h1>{usr}{id}</h1>"
+@app.route("/<usr><region>")
+def user(usr, region):
+    return f"<h1>{usr}{region}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
